71b9a57d49c0ee7b6d6e7de83a26b1baeb289b5f
Jaya Tiwari <jaya.tiwari@example.com>
2026-06-29 22:34:35 -0700

    first commit

---
+++ Frontend/login.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+
+    document.querySelector("form").addEventListener("submit", function(e) {
+
+        e.preventDefault();
+
+        alert("Login Successfully!");
+
+    });
+
+};
